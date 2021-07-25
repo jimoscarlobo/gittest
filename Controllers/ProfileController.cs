5dd7289509c397560cbb41930752e9f798aa26eb
JIM OSCAR LOBO <jimoscarlobo@gmail.com>
2021-07-25 16:29:36 +0100

    added profile controller

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gittest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProfileController : ControllerBase
+    {
+     
+
+        [HttpGet]
+        public List<string> GetTermsUrl()
+        {
+            List<string> terms = new List<string>();
+            terms.Add("https://www.nbg.gov.ge/index.php?m=2");
+            terms.Add("https://www.fms.gov.ge/");
+
+            return terms;
+        }
+    }
+}
