7df64f2871ebdf69b3fed20772f2e68ef8b98bc0
JIM OSCAR LOBO <jimoscarlobo@gmail.com>
2021-07-25 18:38:26 +0100

    added  test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gittest
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        public List<string> GetTest()
+        {
+            List<string> terms = new List<string>();
+            terms.Add("https://www.test.com");
+            terms.Add("https://test.com");
+
+            return terms;
+        }
+    }
+}
