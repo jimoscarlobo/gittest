cc101a912b91a89c023cf278262262f042e94570
JIM OSCAR LOBO <jimoscarlobo@gmail.com>
2021-07-25 16:37:41 +0100

    added home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gittest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> GetMYUrl()
+        {
+            List<string> terms = new List<string>();
+            terms.Add("https://www.google.com");
+            terms.Add("https://facebook.com");
+
+            return terms;
+        }
+    }
+}
